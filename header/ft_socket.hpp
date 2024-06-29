8fd3d8258193324cf7b2673094d011db17f08599
Tom Lasseret <tlassere@k0r2p9.42mulhouse.fr>
2024-06-29 17:59:52 +0200

    Feat: clean function

--- header/ft_socket.hpp
+++ header/ft_socket.hpp
@@ -6,7 +6,7 @@
 /*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 00:53:39 by tlassere          #+#    #+#             */
-/*   Updated: 2024/06/29 02:21:56 by tlassere         ###   ########.fr       */
+/*   Updated: 2024/06/29 17:42:27 by tlassere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,6 @@
 # include "ft_error.hpp"
 
 # define SIZE_QUEUE 100
-
-void	ft_add_client(int const socket_fd, std::vector<int>& client);
-void	ft_free_client(std::vector<int>& client);
-void	ft_get_message(std::vector<int>& client);
 
 int		ft_setsoket(int const socket_fd);
 int		ft_socket_bind(int const socket_fd);
--- src/ft_socket.cpp
+++ src/ft_socket.cpp
@@ -6,7 +6,7 @@
 /*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 00:53:28 by tlassere          #+#    #+#             */
-/*   Updated: 2024/06/29 15:04:09 by tlassere         ###   ########.fr       */
+/*   Updated: 2024/06/29 17:42:10 by tlassere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,94 +55,4 @@ int	ft_socket_bind(int const socket_fd)
 	}
 	ft_error_print(status);
 	return (status);
-}
-
-void	ft_add_client(int const socket_fd, std::vector<int>& client)
-{
-	int	new_clien;
-	
-	new_clien = accept(socket_fd, NULL, 0);
-	// a changer car la c'est pour du test
-	if (new_clien != -1)
-	{
-		client.push_back(new_clien);
-		std::cout << "client fd: " << new_clien << std::endl;
-	}
-}
-
-void	ft_free_client(std::vector<int>& client)
-{
-	std::vector<int>::iterator it;
-
-	it = client.begin();
-	while (it != client.end())
-	{
-		if (*it != -1)
-			close(*it);
-		it++;
-	}
-	client.clear();
-}
-
-static void	ft_get_message_recv(int const client_fd)
-{
-	int		ret_recv;
-	char	buffer[1000] = {0};
-
-	ret_recv = recv(client_fd, buffer, 1000, MSG_DONTWAIT);
-	if (ret_recv > 0)
-		send(client_fd, buffer, 1000, 0); // s'il send alors qu'il n'y a pas de connection ca fait tout crash
-	std::cout << buffer;
-}
-
-static int	ft_get_max_fds(std::vector<int> const& client)
-{
-	std::vector<int>::const_iterator	it;
-	int									max_fd;
-	
-	max_fd = -1;
-	it = client.begin();
-	while (it != client.end())
-	{
-		if (max_fd < *it)
-			max_fd = *it;
-		it++;
-	}
-	max_fd++;
-	return (max_fd);
-}
-
-// pour check si le client est fermer il faut just utiliser recv et check son retour de valeur
-void	ft_get_message(std::vector<int>& client)
-{
-	std::vector<int>::const_iterator	it;
-	int									max_fd;
-	fd_set								rfds;
-	struct timeval						tv;
-	
-	max_fd = ft_get_max_fds(client);
-	it = client.begin();
-	if (max_fd)
-	{
-		tv.tv_sec = 0;
-		tv.tv_usec = 0;
-		FD_ZERO(&rfds);
-		while (it != client.end())
-		{
-			FD_SET(*it, &rfds);
-			it++;
-		}
-		it = client.begin();
-		if (select(max_fd, &rfds, NULL, NULL, &tv) > 0)
-		{
-			while (it != client.end())
-			{
-				if (FD_ISSET(*it, &rfds))
-				{
-					ft_get_message_recv(*it);
-				}
-				it++;
-			}
-		}
-	}
 }
