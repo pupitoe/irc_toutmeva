c2e090e12ba4a572029d91e28dce7a72ea966b46
Tom Lasseret <tlassere@k2r1p1.42mulhouse.fr>
2024-07-22 15:37:01 +0200

    Fix: norm v2

--- header/IRCError.hpp
+++ header/IRCError.hpp
@@ -6,7 +6,7 @@
 /*   By: tlassere <tlassere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 15:14:12 by ggiboury          #+#    #+#             */
-/*   Updated: 2024/07/22 15:25:19 by tlassere         ###   ########.fr       */
+/*   Updated: 2024/07/22 15:36:45 by tlassere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,24 @@
 // Don't forget to see if everything is correctly used.
 class IRCError : public std::exception {
 
-    public :
-        virtual ~IRCError(void) throw(); // Verifier ce que c'est !
-        virtual const char  *what() const throw() = 0;
+	public :
+		virtual ~IRCError(void) throw();
+		virtual const char  *what() const throw() = 0;
 
-    class NeedMoreParams {
-        public :
-            const char  *what(void) const throw();
-    };
-    
-    class Passwdmismatch {
-        public :
-            const char  *what(void) const throw();
-    };
-    
-    class AlreadyRegistered {
-        public :
-            const char  *what(void) const throw();
-    };
+		class NeedMoreParams {
+			public :
+				const char	*what(void) const throw();
+		};
+
+		class Passwdmismatch {
+			public :
+				const char	*what(void) const throw();
+		};
+
+		class AlreadyRegistered {
+			public :
+				const char	*what(void) const throw();
+		};
 };
 
 #endif
